eb4e3c4e7d52fc82366e0dcd551e72e2fc3ca214
BernieDoesYT <janisalinauskis2008@gmail.com>
2025-09-28 01:22:55 +0300

    Update config.h

--- inc/config.h
+++ inc/config.h
@@ -436,7 +436,7 @@
   #define FILTER              0.1  // lower value == softer filter. do not use values <0.01, you will get float precision issues.
 #endif
 #ifndef SPEED_COEFFICIENT
-  #define SPEED_COEFFICIENT   0.5  // higher value == stronger. 0.0 to ~2.0?
+  #define SPEED_COEFFICIENT   2  // higher value == stronger. 0.0 to ~2.0?
 #endif
 #ifndef STEER_COEFFICIENT
   #define STEER_COEFFICIENT   0.5  // higher value == stronger. if you do not want any steering, set it to 0.0; 0.0 to 1.0
@@ -451,7 +451,7 @@
   #define SWITCH_WHEELS       0    // switch right and left wheel. Watch out, you probably also need to invert directions.
 #endif
 #ifndef BEEPS_BACKWARD
-  #define BEEPS_BACKWARD 0    // 0 or 1
+  #define BEEPS_BACKWARD 1    // 0 or 1
 #endif
 
 
